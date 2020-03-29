6585422441c1cd1d44570be688e3ef3b683f9ebd
suwa <starcrabbs@gmail.com>
2020-03-29 02:41:38 -0700

    nah, keep goin

--- kbase.asm
+++ kbase.asm
@@ -2,9 +2,54 @@
 KBSIZE EQU $0400
 SECTION "kbase",ROM0
 memcpy:
-	; DE = size
+	; DE = count
 	; HL = dst
 	; BC = src
 .loop:
+	; copy & increment
 	LD A,[BC]
-	LD [BC],A
+	LDI [HL],A
+	INC BC
+	; decrease count
+	DEC DE
+.check:
+	LD A,E
+	CP $00
+	JR NZ,.loop
+	LD A,D
+	CP $00
+	JR NZ,.loop
+	RET
+
+memset:
+	; DE = count
+	; HL = dst
+	; B = byte to set with
+.loop:
+	LD A,B
+	LDI [HL],A
+	DEC DE
+.check:
+	LD A,E
+	CP $00
+	JR NZ,.loop
+	LD A,D
+	CP $00
+	JR NZ,.loop
+	RET
+
+strcpy:
+	; HL = dst
+	; BC = src
+	PUSH HL
+	PUSH BC
+.loop:
+	LD A,[BC]
+	LDI [HL],A
+	INC BC
+	AND A
+	JR NZ,.loop
+	POP BC
+	POP HL
+	RET
+
--- main.asm
+++ main.asm
@@ -12,21 +12,42 @@ entrypoint:
 ; data
 SECTION "data",ROM0[$0150]
 testtxt:
-	DW .testtxt_0
-	DW .testtxt_1
-.testtxt_0:
+	DW .txt0
+	DW .txt1
+.txt0:
 	DB "oh! create!",0
-.testtxt_1:
+.txt1:
 	DB "ah? remove?",0
 
 testfont:
 	INCBIN "gfx/font.chr"
+testfont_end:
 
 ; main code
 SECTION "main",ROM0
 start:
 	CALL .init_sys
+	CALL .init_fnt
 	JP main_loop
+
+.init_fnt:
+	; copy char to vram
+	LD BC,testfont
+	LD HL,_VRAM
+	LD DE,($10 * $80) ; 28 tiles
+	CALL memcpy
+	; copy text to vram
+	LD BC,testtxt.txt0
+	LD HL,_SCRN0
+	CALL strcpy
+	; copy to wram
+	LD HL,_RAM
+	LD BC,testtxt.txt1
+	CALL strcpy
+	; turn on lcd
+	LD A, LCDCF_ON | LCDCF_BGON | LCDCF_BG8000
+	LD [rLCDC],A
+	RET
 
 .init_sys:
 	; turn off LCD
@@ -41,45 +62,24 @@ start:
 	LD [rOBP0],A
 	LD [rOBP1],A
 	; initialize misc memory
-	CALL .init_wram
-	CALL .init_oam
-	; enable interrupts
-	EI
-	LD A,STATF_VB
-	LD [rSTAT],A
-	; enable LCD again
-	LD A, LCDCF_ON | LCDCF_BGON
+	CALL .init_ram
 	RET
-.init_wram:
+.init_ram:
+	; WRAM clear
 	LD HL,_RAM
-	LD DE,$0000
-.init_wram_loop:
-	; load mem & increment
-	LD A,$00
-	LD [HL+],A
-	INC DE
-	; compare hi
-	LD A,D
-	CP A,$20
-	JR C,.init_wram_loop
-	RET
-.init_oam:
+	LD DE,$2000
+	LD B,$FF
+	CALL memset
+	; VRAM clear
+	LD HL,_VRAM
+	LD DE,$2000
+	LD B,$00
+	CALL memset
 	RET
 
 main_loop:
 	; do bullshit
-	LD HL,_VRAM
-	LD A,[HL]
-	INC A
-	LD [HL],A
+	
 	; wait
-	LD A,LCDCF_ON | LCDCF_BGON | LCDCF_BG8000
-	CALL .vbl_wait
 	jr main_loop
-
-.vbl_wait:
-	LD a,[rLY]
-	CP A,144
-	JR NZ,.vbl_wait
-	RET
 
